1d9e778187614a7e386c90a0fa8478b2ae45cc64
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2018-04-10 23:49:06 +0300

    added bot api, config; impl sendPhoto

--- app.js
+++ app.js
@@ -5,15 +5,7 @@ const Telegraf = require('telegraf');
 const Screenshot = require('./src/screenshot/screenshot');
 const fs = require('fs');
 
-// Screenshot.getScreenshot('https://magicseaweed.com/Hilton-Surf-Report/3658/', 'forcast', __dirname).then(
-//   (path) => {
-//     console.log(path);
-//   }, (err) => {
-//     console.log(err);
-//   });
-
 const handleForcastReq = function(ctx) {
-  // ctx.reply("Working...");
   Screenshot.getScreenshot('https://magicseaweed.com/Hilton-Surf-Report/3658/', 'forcast', __dirname).then(
     (path) => {
       ctx.replyWithPhoto({
@@ -43,52 +35,4 @@ bot.telegram.getMe().then(function(me)
     bot.command('/getForcast@' + botUsername, (ctx) => handleForcastReq(ctx));
 });
 bot.command('/getForcast', (ctx) => handleForcastReq(ctx));
-bot.command('/getme', (ctx) => { ctx.reply("username: " + botUsername); });
-
-// bot.command('/getForcast', (ctx) => {
-//   ctx.telegram.sendPhoto(ctx.message.chat.id , fs.readFileSync('./forcast.png'), {
-//     caption: 'Caption *text*',
-//     parse_mode: 'Markdown'
-//   }).catch((error) => {
-//     console.log(error.code);  // => 'ETELEGRAM'
-//     console.log(error.response.description); // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
-//     process.exit();
-//     });
-// });
-
-// bot.command('/getForcast', (ctx) => {
-//   const buffer = fs.readFileSync('./forcast.png');
-//   ctx.replyWithPhoto(buffer, {
-//     caption: 'Caption *text*',
-//     parse_mode: 'Markdown'
-//   }, {
-//     filename: 'forcast.png',
-//     contentType: 'image/png'
-//   }
-// ).catch((error) => {
-//   console.log(error.code);  // => 'ETELEGRAM'
-//   console.log(error.response.description); // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
-//   process.exit();
-//   });
-// });
-
-// sendPhoto(msg.chat.id,"https://www.somesite.com/image.jpg",{caption : "Here we go ! \nThis is just a caption "} );
-// bot.command('/getForcast', (ctx) => {
-//   ctx.telegram.sendPhoto({
-//     chatId: ctx.message.chat.id,
-//     caption: 'Test caption',
-//     photo: './forcast.png',
-//     // photo: fs.createReadStream('./forcast.png')
-//   }, function(err, msg) {
-//     console.log(err);
-//     console.log(msg);
-//   });
-// });
-
-//
-// replyWithPhoto({
-//   source: fs.createReadStream('./forcast.png'),
-//   caption: 'test',
-//   parse_mode: 'Markdown'
-// }));
 bot.startPolling();
